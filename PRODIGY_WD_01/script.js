3869d6683db087f8dc412ffcc6b0151b1bf3df19
ChaithraN7395 <chaithra72311@gmail.com>
2025-01-24 21:22:53 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,10 @@
+// Add navbar scroll effect
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
